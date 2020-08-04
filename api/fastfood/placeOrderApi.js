4ce828178c4f4da164f8205ee96a39e5a089042f
Vishwajeet Kumar Singh <35272198+vsrnitp@users.noreply.github.com>
2020-08-05 01:28:21 +0530

    Update placeOrderApi.js

--- api/fastfood/placeOrderApi.js
+++ api/fastfood/placeOrderApi.js
@@ -26,7 +26,17 @@ router.post('/confirm',(req,res)=>{
         if(err) res.send(err);
         else{
             res.status(200).send(doc);
-            // here we will set the logic for twillio to send sms...
+            // here we will set the logic for twilio to send sms...
+            const accountSid = 'AC7b052f17d35fd1cb6a96015095830233';
+            const authToken  = 'ef8877c01a862c95b98faa11bdc40c4c';
+            const client = require('twilio')(accountSid,authToken);
+            client.messages
+                .create({
+                    body:'Your order for '+req.body.productName+' has been placed successfully!',
+                    from:'+12057720546',
+                    to:req.body.customerMobileNo'
+            })
+            .then(message => console.log(message));
         }
     })
 })
