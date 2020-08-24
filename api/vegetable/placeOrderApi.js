e868756c689d320bfe7ba95dc23a819b08ec2ec3
Vishwajeet Kumar Singh <35272198+vsrnitp@users.noreply.github.com>
2020-08-24 10:15:12 +0530

    Update placeOrderApi.js

--- api/vegetable/placeOrderApi.js
+++ api/vegetable/placeOrderApi.js
@@ -26,17 +26,40 @@ router.post('/vegetableConfirm',(req,res)=>{
         if(err) res.send(err);
         else{
             res.status(200).send(doc);
-            // here we will set the logic for twilio to send sms...
-            const accountSid = 'AC7b052f17d35fd1cb6a96015095830233';
-            const authToken  = 'ef8877c01a862c95b98faa11bdc40c4c';
-            const client = require('twilio')(accountSid,authToken);
-            client.messages
-                .create({
-                    body:'Your order for '+req.body.productName+' has been placed successfully!',
-                    from:'+12057720546',
-                    to:'+918209186342'
-            })
-            .then(message => console.log(message));
+            // here we will set the logic for nodemailer to send sms...
+           /****************NODEMAILER FROM HERE********** */    
+        // creating transporter for nodemailer
+        let transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+              user: "coolvsr6@gmail.com", // generated ethereal user//host user email or admin email(fill email and password)
+              pass: "Tim@2786" // generated ethereal password//admin password
+            },tls:{
+              rejectUnauthorised:true
+            }
+          });
+
+                // setup email data with unicode symbols
+           // var link="http://"+req.get('host')+"/verify?id="+u_email;//verification link or the path of the user
+            let mailOptions = {
+            from: '"nodemailer👻" <coolvsr6@gmail.com>', // sender address
+            to: req.body.email, // list of receivers
+            subject: "no_reply just verify your account to be our member", // Subject line
+            text: "Hello world? Your mail is verified...", // plain text body
+            };
+            
+            //sending the mail
+            transporter.sendMail(mailOptions,function(err,info){
+                if(err)
+                console.log(err);
+                else {
+                  console.log("SUCCESS IN SENIDNG THE MAIL FROM NODE nodemailer");
+                }
+              });
+
+              /*******************NODEMAILER ENDS HERE*************** */
         }
     })
 })
