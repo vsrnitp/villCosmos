c15a67713696234d2af4c74af8a1492a0d4fbd93
Vishwajeet Kumar Singh <35272198+vsrnitp@users.noreply.github.com>
2020-08-24 11:37:21 +0530

    Update placeOrderApi.js

--- api/vegetable/placeOrderApi.js
+++ api/vegetable/placeOrderApi.js
@@ -27,39 +27,7 @@ router.post('/vegetableConfirm',(req,res)=>{
         else{
             res.status(200).send(doc);
             // here we will set the logic for nodemailer to send sms...
-           /****************NODEMAILER FROM HERE********** */    
-        // creating transporter for nodemailer
-        let transporter = nodemailer.createTransport({
-            host: "smtp.gmail.com",
-            port: 587,
-            secure: false, // true for 465, false for other ports
-            auth: {
-              user: "coolvsr6@gmail.com", // generated ethereal user//host user email or admin email(fill email and password)
-              pass: "Tim@2786" // generated ethereal password//admin password
-            },tls:{
-              rejectUnauthorised:true
-            }
-          });
-
-                // setup email data with unicode symbols
-           // var link="http://"+req.get('host')+"/verify?id="+u_email;//verification link or the path of the user
-            let mailOptions = {
-            from: '"nodemailer👻" <coolvsr6@gmail.com>', // sender address
-            to: 'vsrnitp@gmail.com', // list of receivers
-            subject: "no_reply just verify your account to be our member", // Subject line
-            text: "Hello world? Your mail is verified...", // plain text body
-            };
-            
-            //sending the mail
-            transporter.sendMail(mailOptions,function(err,info){
-                if(err)
-                console.log(err);
-                else {
-                  console.log("SUCCESS IN SENIDNG THE MAIL FROM NODE nodemailer");
-                }
-              });
-
-              /*******************NODEMAILER ENDS HERE*************** */
+        
         }
     })
 })
